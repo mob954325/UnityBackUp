fe5d575969f75953fbecdf1585b62c3aaa831276
mob <lsh954325@gmail.com>
2024-01-11 00:01:27 +0900

    적 드론 움직임 추가
    raycast를 이용해 ground layout 유무로 패트롤 / 24.01.11

--- 2D_Platformer/Assets/Scenes/Scripts/Enemy/Enemy_Drone.cs
+++ 2D_Platformer/Assets/Scenes/Scripts/Enemy/Enemy_Drone.cs
@@ -10,8 +10,18 @@ public class Enemy_Drone : Enemy
     public LayerMask _targetMast;
     private RaycastHit2D _rayTarget;
 
+    [Header("#Stat Info")]
+    public Vector3 _moveDir = Vector3.zero;
+    public float _speed;
+
+    void Awake()
+    {
+        _moveDir = Vector3.right;
+    }
+
     void Update()
     {
+        transform.Translate(Time.deltaTime * _moveDir * _speed);
         CheckGround();
     }
 
@@ -21,7 +31,7 @@ public class Enemy_Drone : Enemy
     void CheckGround()
     {
         Vector2 _startRay = new Vector3(transform.position.x + _rayOffsetX,
-                                transform.position.y);
+                                        transform.position.y);
         Vector2 _endRay = new Vector3(_startRay.x, _startRay.y + _rayOffsetY);
 
         _rayTarget = Physics2D.Linecast(_startRay, _endRay, _targetMast);
@@ -30,6 +40,11 @@ public class Enemy_Drone : Enemy
         if (_rayTarget.collider != null && _rayTarget.collider.gameObject.CompareTag("Ground"))
         {
             Debug.Log($"tag : {_rayTarget.collider.gameObject.tag}");
+        }
+        else if(_rayTarget.collider == null)
+        {
+            _rayOffsetX *= -1f;
+            _speed *= -1f;
         }
     }
 }
